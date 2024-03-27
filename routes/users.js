d4b796c73d718f3e5f4cd0d416206ceab04939ac
Samadali123 <samadali0125@gmail.com>
2024-03-27 14:56:40 +0530

    upload files

---
+++ routes/users.js
@@ -0,0 +1,47 @@
+const mongoose = require(`mongoose`);
+
+mongoose.connect("mongodb://0.0.0.0:27017/Pinterest");
+
+const passportlocalmongoose = require(`passport-local-mongoose`);
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+    },
+    email: {
+        type: String,
+    },
+    password: {
+        type: String,
+    },
+    profile: String,
+    fullname: String,
+    firstname: String,
+    lastname: String,
+    About: String,
+    website: String,
+    pins: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: `pins`,
+    }, ],
+    savedpins: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: `pins`,
+    }, ],
+
+    followers: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: `users`,
+    }, ],
+
+    following: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: `users`,
+    }, ],
+
+    socketId: String,
+});
+
+userSchema.plugin(passportlocalmongoose);
+
+module.exports = mongoose.model("users", userSchema);
